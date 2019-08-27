72d2ec49362b603842121ccffa2b74f659413b58
Ionutz <ionutzme3@gmail.com>
2019-08-27 10:19:25 +0300

    Updates

---
+++ precache-manifest.f5b34ead33884b7169ed595b3f0896af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae91e014c75b582552ab54ef830a3209",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "87a1d7294e7d831722ff",
+    "url": "/monsters-rolodex/static/css/main.33c75764.chunk.css"
+  },
+  {
+    "revision": "f802219fca84ad4640ab",
+    "url": "/monsters-rolodex/static/js/2.ea1710b8.chunk.js"
+  },
+  {
+    "revision": "87a1d7294e7d831722ff",
+    "url": "/monsters-rolodex/static/js/main.4d8d42b6.chunk.js"
+  },
+  {
+    "revision": "def15b5fa272a5a7fbd0",
+    "url": "/monsters-rolodex/static/js/runtime~main.a19a323f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
